8c1fba91eef9c2ff8ca37a52114af32fe25e9384
tnraddatz <tnraddatz@email.wm.edu>
2019-04-08 22:35:01 -0400

    Initial Commit

---
+++ DotNet/Udemy/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Udemy.Models;
+
+namespace Udemy.Data
+{   //Inherit From DbContext
+    public class DataContext : DbContext 
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Value> Values { get; set; }
+
+    }
+}
---
+++ DotNet/Udemy/Migrations/20190407170432_InitialMigration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+//Migrations are run with Add-Migration InitialMigrations && Update-Database
+namespace Udemy.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Values",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    MyProperty = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Values", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Values");
+        }
+    }
+}
